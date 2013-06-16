66d40b0b9a35fac4ed895c56e8312af6c7974a97
agiertli <agiertli@redhat.com>
2013-06-16 11:27:34 +0200

    typo in even/dao/test

--- peaktogether-ejb/src/test/java/pv243/peaktogether/test/dao/EventDAOTest.java
+++ peaktogether-ejb/src/test/java/pv243/peaktogether/test/dao/EventDAOTest.java
@@ -1,5 +1,9 @@
 package pv243.peaktogether.test.dao;
 
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -13,20 +17,35 @@ import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.DependencyResolvers;
 import org.jboss.shrinkwrap.resolver.api.maven.MavenDependencyResolver;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+
 import pv243.peaktogether.TestUtils;
+import pv243.peaktogether.dao.EventDAOInt;
 import pv243.peaktogether.dao.MemberDAOInt;
+import pv243.peaktogether.model.Event;
+import pv243.peaktogether.model.Location;
+import pv243.peaktogether.model.LocationType;
+import pv243.peaktogether.model.Member;
 import pv243.peaktogether.model.Photo;
+import pv243.peaktogether.model.Skill;
+import pv243.peaktogether.model.Sport;
 
 @RunWith(Arquillian.class)
-public class EvenDAOTest {
+public class EventDAOTest {
 
 	@PersistenceContext
 	private EntityManager em;
 	@Inject
 	private UserTransaction utx;
+	@Inject
+	private EventDAOInt eventDAO;
+	@Inject
+	private MemberDAOInt memberDAO;
 
 	@Deployment
 	public static Archive<?> createTestArchive() {
@@ -59,6 +78,57 @@ public class EvenDAOTest {
 
 	@Test
 	public void testRemove() throws Exception {
+		
+		
+		
+		Set<Member> friends1 = new HashSet<Member>();
+		Member member1 = new Member();
+		member1.setEmail("agiertli@gmail.com");
+		member1.setUsername("jonas");
+		member1.setRegistered(new Date());
+		memberDAO.create(member1);
+		
+		friends1.add(member1);
+		
+		Skill skill1 = new Skill();
+		Set<Skill> skills1 = new HashSet<Skill>();
+		skill1.setSport(Sport.SKIING);
+		skill1.setLevel(10);
+		skills1.add(skill1);
+		Member member = new Member();
+		member.setEmail("respectx@gmail.com");
+		member.setUsername("anton");
+		member.setRegistered(new Date());
+		member.setFriends(friends1);
+		member.setSkills(skills1);
+		
+		memberDAO.create(member);
+		
+		
+		Set<Location> locations1 = new HashSet<Location>();
+		Location location1 = new Location();
+		location1.setType(LocationType.START);
+		GeometryFactory gf = new GeometryFactory();
+		location1.setPoint(gf.createPoint(new Coordinate(5,4)));
+		Event event1 = new Event();
+		event1.setCapacity(37);
+		event1.setDescription("Event description.");
+		event1.setName("A nice skiing trip.");
+		event1.setPublicEvent(true);
+		event1.setOwner(member);
+		event1.setLimited(true);
+		event1.setStart(new Date(new Date().getTime() + 10000));
+		event1.setLocations(locations1);
+		
+		eventDAO.create(event1);
+		
+		Event found = eventDAO.findById(event1.getId());
+		Assert.assertNotNull("Event not found",found);
+		
+		eventDAO.remove(found);
+		
+		Assert.assertNull("Nasli sme event, aj ked tam uz nema byt",eventDAO.findById(found.getId()));
+		
 
 	}
 
